b5e9c214c2c2de2a3e78c94d9d283c4bc945dcb0
Liv Johnson <livjab@gmail.com>
2019-06-18 16:30:13 -0500

    followed steps from  aarons lecture

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,16 +1,25 @@
 """ Main application and routing logic for TwitOff. """
-
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """ Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route("/")
     def root():
-        return 'Welcome to TwitOff!'
+        users = User.query.all()
+        return render_template('base.html', title="Home", users=users)
+
+    @app.route('/reset') # remove for production
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
 
     return app
